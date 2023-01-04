8c984ed6e3449064b5b67c047008f364beb968ae
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-04 19:02:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ from config import (
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
     FORCE_SUB_GROUP2,
-    FORCE_SUB_GROUP3,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -98,26 +97,6 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_GROUP2}"
-                )
-                self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/ramsupportt untuk Bantuan"
-                )
-                sys.exit()
-        
-        if FORCE_SUB_GROUP3:
-            try:
-                link = (await self.get_chat(FORCE_SUB_GROUP3)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_GROUP3)
-                    link = (await self.get_chat(FORCE_SUB_GROUP3)).invite_link
-                self.invitelink4 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_GROUP3!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_GROUP3}"
                 )
                 self.LOGGER(__name__).info(
                     "Bot Berhenti. Gabung Group https://t.me/ramsupportt untuk Bantuan"
